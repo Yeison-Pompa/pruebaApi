28005e6beef4e7920dd0b6192754e3436d8f420d
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-07-07 18:19:54 -0500

    add grafico

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,45 @@
 const btnBuscar = document.querySelector("#buscar");
 
-const getMoneda = async () => {
-  const res = await fetch(`https://mindicador.cl/api`);
+const getMoneda = async (monedaValue) => {
+  const res = await fetch(`https://mindicador.cl/api/${monedaValue}`);
   const data = await res.json();
   return data;
 };
 
+const graficoMoneda = async (series) => {
+  console.log(series);
+  const monto = (series.map = (serie) => serie.valor);
+  const fecha = (series.map = (serie) => serie.fecha);
+
+  const ctx = document.getElementById("myChart");
+
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: fecha,
+      datasets: [
+        {
+          label: "# of Votes",
+          data: monto,
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
+};
+
 btnBuscar.addEventListener("click", async () => {
+  let grafico = Chart.getchart("mychart");
+  if (grafico !== undefined) {
+    grafico.destroy();
+  }
+
   const inputValue = document.querySelector("#input").value;
   const monedaValue = document.querySelector("#moneda").value;
-  const respuesta = await getMoneda();
+  const respuesta = await getMoneda(monedaValue);
+  console.log(respuesta.serie[0].valor);
+  let tipoCambio = respuesta.serie[0].valor * inputValue;
 
-  let tipoCambio = inputValue * respuesta[monedaValue].valor;
+  graficoMoneda(respuesta.serie);
   document.querySelector("#resultado").innerHTML = tipoCambio;
 });
