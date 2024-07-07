73d8b1843803450fdf786cde2b9a26b22f59f04b
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-07-07 16:46:37 -0500

    modificando el codigo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,16 @@
 const btnBuscar = document.querySelector("#buscar");
 
-btnBuscar.addEventListener("click",()=>{
- const inputValue = document.querySelector("#input").value;
- const monedaValue = document.querySelector("#moneda").value;
- console.log(inputValue);
- console.log(monedaValue);
+const getMoneda = async () => {
+  const res = await fetch(`https://mindicador.cl/api`);
+  const data = await res.json();
+  return data;
+};
 
+btnBuscar.addEventListener("click", async () => {
+  const inputValue = document.querySelector("#input").value;
+  const monedaValue = document.querySelector("#moneda").value;
+  const respuesta = await getMoneda();
 
-})
+  let tipoCambio = inputValue * respuesta[monedaValue].valor;
+  document.querySelector("#resultado").innerHTML = tipoCambio;
+});
