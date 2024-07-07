aa5b6b254add012efee2255301c95cef469773fd
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-07-07 12:46:09 -0500

    add variables y eventos

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+const btnBuscar = document.querySelector("#buscar");
+
+btnBuscar.addEventListener("click",()=>{
+ const inputValue = document.querySelector("#input").value;
+ const monedaValue = document.querySelector("#moneda").value;
+ console.log(inputValue);
+ console.log(monedaValue);
+
+
+})
